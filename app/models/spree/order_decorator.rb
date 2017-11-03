7ee8c1bbb5c8c44ff1e7bd486a801f9f139b28eb
manish rai <manish.rai@w3villa.com>
2017-11-03 12:51:56 +0530

    dynamic recipient

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -5,7 +5,17 @@ module Spree
 		has_many :children_orders, class_name: "Spree::Order", foreign_key: :parent_id
 		# ********************* Methods *********************************************
 
-		
+		def get_recipients
+			recipient_arr = []
+			if children_orders.present?
+				recipient_arr = children_orders.collect(&:recipient_name)
+			end
+			unless recipient_arr.include?('Me')
+				recipient_arr = recipient_arr.push('Me')
+			end
+			recipient_arr = recipient_arr.push('Recipient')
+			return recipient_arr
+		end
 		
 	end
 end
